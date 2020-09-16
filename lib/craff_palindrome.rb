a846bd3c06a2175f109fc5433f073f435039204a
Cathy R <cat@catr.co>
2020-09-16 16:27:20 +1000

    Initialise repository

---
+++ lib/craff_palindrome.rb
@@ -0,0 +1,6 @@
+require "craff_palindrome/version"
+
+module CraffPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/craff_palindrome/version.rb
@@ -0,0 +1,3 @@
+module CraffPalindrome
+  VERSION = "0.1.0"
+end
