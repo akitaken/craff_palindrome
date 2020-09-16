8f4e64a683c627064d112763ca872cd4fb295c37
Cathy R <cat@catr.co>
2020-09-16 18:10:02 +1000

    Expand tester to include integers

--- lib/craff_palindrome.rb
+++ lib/craff_palindrome.rb
@@ -1,18 +1,28 @@
 require "craff_palindrome/version"
 
-class String
-
+module CraffPalindrome
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
   end
 
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      letter_regex = /[a-z]/i
-      self.scan(letter_regex).join.downcase
+      letter_regex = /[a-z0-9]/i
+      self.to_s.scan(letter_regex).join.downcase
     end
+end
 
+class String
+  include CraffPalindrome
+end
+
+class Integer
+  include CraffPalindrome
 end
